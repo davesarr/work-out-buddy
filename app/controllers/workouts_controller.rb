c06bed6564b2bdbdca1dbfab9646482e2baf7b8e
Daouda Sarr <davesarr@hotmail.com>
2016-09-23 23:04:12 -0400

    adding links and edit workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -25,9 +25,16 @@ def  new
   end
 
   def update
+    if @workout.update(workout_params)
+      redirect_to @workout
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @workout.destroy
+    redirect_to root_path
   end
 
   private
