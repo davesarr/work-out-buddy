f4766e35911c43e1cb8c6f68e9f70b4e5f3ceb45
Daouda Sarr <davesarr@hotmail.com>
2016-09-23 21:44:23 -0400

    single workout page

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,8 @@
 class WorkoutsController < ApplicationController
+  before_action :find_workout, only: [:show, :edit, :update, :destroy]
+
  def index
+  @workouts = Workout.all.order("created_at DESC")
   end
 
   def show
@@ -33,6 +36,7 @@ def  new
     params.require(:workout).permit(:date, :workout, :energy, :length)
     end
   def find_workout
+    @workout = Workout.find(params[:id])
   end
 
 end
